a02f18c6cc15abac6fda65f8591043a3c8bb37a5
Brayan <brayan_103@outlook.com>
2018-09-25 19:50:39 -0500

    "flask +"

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask , render_template
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,8 +17,28 @@ def hello(name):
 
 @app.route('/index')
 def index():
-    return render_template("hello.html")
+    return render_template("index.html",hola="HOLA!")
 
 @app.route('/link')
 def link():
     return render_template("link.html")
+
+@app.route('/new_year')
+def new_year():
+    gg = datetime.datetime.now()
+    ny = gg.month == 1 and gg.day == 1
+
+    return render_template("index.html",new_year=ny,headline = "IS IT NEW YEAR?")
+
+@app.route('/loop')
+def loop():
+    lista = ["brayan0","brayan1","brayan2"]
+    return render_template("index.html",names = lista)
+
+@app.route('/hijo1')
+def hijo1():
+    return render_template("hijo1.html")
+
+@app.route('/hijo2')
+def hijo2():
+    return render_template("hijo2.html")
